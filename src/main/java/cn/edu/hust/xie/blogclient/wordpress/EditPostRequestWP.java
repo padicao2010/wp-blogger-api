2239848e0de69088910aca48eac7d19346895a71
ywxie <applepiepine@yahoo.com>
2015-03-03 12:01:38 +0800

    增加几个WordPress的XML-RPC API，新建，编辑，删除文章

---
+++ src/main/java/cn/edu/hust/xie/blogclient/wordpress/EditPostRequestWP.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 padicao.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.edu.hust.xie.blogclient.wordpress;
+
+import cn.edu.hust.xie.blogclient.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author padicao
+ */
+public class EditPostRequestWP extends Request {
+
+    private static final String METHOD_NAME = "wp.editPost";
+    
+    public EditPostRequestWP(BlogCore c, PostInfo p) {
+        super(METHOD_NAME);
+        params = new RequestParam[5];
+        params[0] = c.getBlogId();
+        params[1] = c.getUserName();
+        params[2] = c.getPassWord();
+        params[3] = new RequestParam(XMLTags.I4, p.postId);
+        Map<String, RequestParam> map = new HashMap<String, RequestParam>();
+        map.put("post_title", new RequestParam(XMLTags.STRING, p.title));
+        map.put("post_status", new RequestParam(XMLTags.STRING, p.status));
+        map.put("post_content", new RequestParam(XMLTags.STRING, p.content));
+        params[4] = new RequestParam(XMLTags.STRUCT, map);
+    }
+    
+    @Override
+    public Response getResponseInstance() {
+        return new EditPostResponseWP();
+    }
+    
+}
